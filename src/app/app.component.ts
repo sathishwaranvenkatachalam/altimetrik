e0cd440fbcf2b6dfe7bb9fe0db5217d6f39fa091
sathishwaranvenkatachalam <sathishwaran75@gmail.com>
2021-05-25 17:30:04 +0530

    assignment initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'altimetrik';
+  documents = [
+    {name: 'Terminal instructables', category: 'Techinical', color: 'rgb(237 156 223 / 35%)'},
+    {name: 'Installation guides', category: 'Techinical', color: 'rgb(237 156 223 / 35%);'},
+    {name: 'Career opportunities', category: 'Human resources', color: 'rgb(11 181 110 / 24%)'},
+    {name: 'Customer onboarding', category: 'Financial', color: 'rgb(135 247 170 / 41%);'},
+    {name: 'Outlet branding', category: 'Marketing', color: 'rgba(241, 156, 156, 0.5)'},
+    {name: 'Accounting procedures', category: 'Financial', color: ''},
+    {name: 'Training', category: 'Human resources', color: 'rgb(11 181 110 / 24%)'},
+    {name: 'Printing guidance', category: 'Marketing', color: 'rgba(241, 156, 156, 0.5)'},
+    {name: 'Technical support', category: 'Techinical', color: 'rgb(237 156 223 / 35%);'}
+  ]
 }
